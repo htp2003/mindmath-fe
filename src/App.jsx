a22a3785f8e7b5d4a17e69051ea99c234187f034
htp2003 <hatanphat08102003@gmail.com>
2024-10-07 20:00:39 +0700

    main first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
